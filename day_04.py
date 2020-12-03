8af69f09e28473d73e25286d74588a5220ecf3df
Caleb Jordan <cjordan@flatiron.com>
2020-12-02 22:25:42 -0700

    Setup for 4

---
+++ day_04.py
@@ -0,0 +1,15 @@
+from solvers import BaseSolver
+
+
+class Solver(BaseSolver):
+    @staticmethod
+    def load_input(f):
+        pass
+
+    @staticmethod
+    def part_1(data):
+        pass
+
+    @staticmethod
+    def part_2(data):
+        pass
