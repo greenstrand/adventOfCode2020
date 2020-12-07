14fa6b77007c64a7238ab53952523463fe4fa4bc
Caleb Jordan <cjordan@flatiron.com>
2020-12-07 16:12:39 -0700

    Day 5: simplify! Don't need rows or cols at all.

--- day_05.py
+++ day_05.py
@@ -5,21 +5,14 @@ from itertools import starmap
 class Solver(DefaultSolver):
     @staticmethod
     def process_input(lines):
-        output = []
-        for line in lines:
-            fb = bits_to_int([x != "F" for x in line[:7]])
-            lr = bits_to_int([x != "L" for x in line[7:]])
-            output.append((fb, lr))
-        return output
+        return [bits_to_int([x in "BR" for x in line]) for line in lines]
 
     @staticmethod
-    def part_1(data):
-        ids = list(starmap(get_id, data))
+    def part_1(ids):
         return max(ids)
 
     @staticmethod
-    def part_2(data):
-        ids = list(starmap(get_id, data))
+    def part_2(ids):
         missing = set(range(min(ids), max(ids) + 1)) - set(ids)
         assert len(missing) == 1
         return missing.pop()
@@ -27,7 +20,3 @@ class Solver(DefaultSolver):
 
 def bits_to_int(bits):
     return sum(int(b) * (1 << i) for i, b in enumerate(reversed(bits)))
-
-
-def get_id(row, col):
-    return row * 8 + col
